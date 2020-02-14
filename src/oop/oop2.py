e1b912c8d42058ab35c3d4e745aeb1fd2b99487f
BaoPham92 <baopham92@yahoo.com>
2020-02-14 12:59:59 -0500

    Minor: Syntax adjustments for MVP

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
-    # TODO
+    @staticmethod
+    def drive():
+        return 'vroooom'
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,8 +18,13 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
 
-# TODO
+    @staticmethod
+    def drive():
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -29,4 +36,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    vehicle.drive()
